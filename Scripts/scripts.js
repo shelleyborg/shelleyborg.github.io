32dbe606a6108afc36d0b8a8e09705e1f37fc8f4
shelleyborg <shelleymaeborg@gmail.com>
2024-04-28 19:00:42 +0200

    Created Video Page
    Started slideshow

---
+++ Scripts/scripts.js
@@ -0,0 +1,18 @@
+var videos = ['Videos/LiveShow.mp4', 'Videos/videoplayback(6).mp4'];
+var currentVideoIndex = 0;
+
+function videoslider(link) {
+    document.querySelector(".slider").src = link;
+    currentVideoIndex = videos.indexOf(link);
+    console.log('current Vide:'+ link)
+}
+
+function nextVideo() {
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    document.querySelector(".slider").src = videos[currentVideoIndex];
+}
+
+function prevVideo() {
+    currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+    document.querySelector(".slider").src = videos[currentVideoIndex];
+}
